ad2b5a604c1058b26e53b87dd85eb8a5243c6c7d
xlumen1 <proxyig74@gmail.com>
2025-06-26 16:31:52 +0000

    Window now renders

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,23 @@ int main() {
 		return -1;
 	}
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Boundforge", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create window.\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
